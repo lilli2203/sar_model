dba3e112586f243ffab3da08a63f61bc3e307b01
lilli2203 <mailyourlilli@gmail.com>
2024-08-04 00:46:51 +0530

    differeantiator

--- model.py
+++ model.py
@@ -385,3 +385,99 @@ def upsample(filters, size, apply_dropout=False):
 up_model = upsample(3, 4)
 up_result = up_model(down_result)
 print (up_result.shape)
+
+
+def Generator():
+  inputs = tf.keras.layers.Input(shape=[256, 256, 3])
+
+  down_stack = [
+    downsample(64, 4, apply_batchnorm=False),  
+    downsample(128, 4),  
+    downsample(256, 4),  
+    downsample(512, 4), 
+    downsample(512, 4), 
+    downsample(512, 4),  
+    downsample(512, 4),  
+    downsample(512, 4),  
+  ]
+
+  up_stack = [
+    upsample(512, 4, apply_dropout=True),  
+    upsample(512, 4, apply_dropout=True),  
+    upsample(512, 4, apply_dropout=True), 
+    upsample(512, 4),  
+    upsample(256, 4),  
+    upsample(128, 4),  
+    upsample(64, 4),  
+  ]
+
+  initializer = tf.random_normal_initializer(0., 0.02)
+  last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
+                                         strides=2,
+                                         padding='same',
+                                         kernel_initializer=initializer,
+                                         activation='tanh')  
+
+  x = inputs
+  skips = []
+  for down in down_stack:
+    x = down(x)
+    skips.append(x)
+
+  skips = reversed(skips[:-1])
+
+  for up, skip in zip(up_stack, skips):
+    x = up(x)
+    x = tf.keras.layers.Concatenate()([x, skip])
+
+  x = last(x)
+
+  return tf.keras.Model(inputs=inputs, outputs=x)
+
+generator = Generator()
+tf.keras.utils.plot_model(generator, show_shapes=True, dpi=64)
+
+def generator_loss(disc_generated_output, gen_output, target):
+  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+
+  l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+  return total_gen_loss, gan_loss, l1_loss
+
+loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+LAMBDA = 100
+
+def Discriminator():
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
+  tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
+
+  x = tf.keras.layers.concatenate([inp, tar])  
+
+  down1 = downsample(64, 4, False)(x)  
+  down2 = downsample(128, 4)(down1)  
+  down3 = downsample(256, 4)(down2)  
+
+  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3) 
+  conv = tf.keras.layers.Conv2D(512, 4, strides=1,
+                                kernel_initializer=initializer,
+                                use_bias=False)(zero_pad1) 
+
+  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+
+  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+
+  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  
+
+  last = tf.keras.layers.Conv2D(1, 4, strides=1,
+                                kernel_initializer=initializer)(zero_pad2)  
+
+  return tf.keras.Model(inputs=[inp, tar], outputs=last)
+
+discriminator = Discriminator()
+tf.keras.utils.plot_model(discriminator, show_shapes=True, dpi=64)
+
