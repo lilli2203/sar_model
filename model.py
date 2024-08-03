617a18c82a55ed52825335d9b0385ab7a0f36e2f
lilli2203 <mailyourlilli@gmail.com>
2024-08-04 00:49:06 +0530

    more updat

--- model.py
+++ model.py
@@ -387,97 +387,175 @@ up_result = up_model(down_result)
 print (up_result.shape)
 
 
+# Constants
+BUFFER_SIZE = 10000
+BATCH_SIZE = 1
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+OUTPUT_CHANNELS = 3
+LAMBDA = 100
+
+# Generator Model
+def downsample(filters, size, apply_batchnorm=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
+    result = tf.keras.Sequential()
+    result.add(tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                                      kernel_initializer=initializer, use_bias=False))
+    if apply_batchnorm:
+        result.add(tf.keras.layers.BatchNormalization())
+    result.add(tf.keras.layers.LeakyReLU())
+    return result
+
+def upsample(filters, size, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0., 0.02)
+    result = tf.keras.Sequential()
+    result.add(tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                               padding='same',
+                                               kernel_initializer=initializer,
+                                               use_bias=False))
+    result.add(tf.keras.layers.BatchNormalization())
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
+    result.add(tf.keras.layers.ReLU())
+    return result
+
 def Generator():
-  inputs = tf.keras.layers.Input(shape=[256, 256, 3])
+    inputs = tf.keras.layers.Input(shape=[256, 256, 3])
+    down_stack = [
+        downsample(64, 4, apply_batchnorm=False), 
+        downsample(128, 4),  
+        downsample(256, 4),  
+        downsample(512, 4),  
+        downsample(512, 4),  
+        downsample(512, 4),  
+        downsample(512, 4),  
+        downsample(512, 4),  
+    ]
 
-  down_stack = [
-    downsample(64, 4, apply_batchnorm=False),  
-    downsample(128, 4),  
-    downsample(256, 4),  
-    downsample(512, 4), 
-    downsample(512, 4), 
-    downsample(512, 4),  
-    downsample(512, 4),  
-    downsample(512, 4),  
-  ]
+    up_stack = [
+        upsample(512, 4, apply_dropout=True), 
+        upsample(512, 4, apply_dropout=True), 
+        upsample(512, 4, apply_dropout=True), 
+        upsample(512, 4),  
+        upsample(256, 4),  
+        upsample(128, 4),  
+        upsample(64, 4),  
+    ]
 
-  up_stack = [
-    upsample(512, 4, apply_dropout=True),  
-    upsample(512, 4, apply_dropout=True),  
-    upsample(512, 4, apply_dropout=True), 
-    upsample(512, 4),  
-    upsample(256, 4),  
-    upsample(128, 4),  
-    upsample(64, 4),  
-  ]
+    initializer = tf.random_normal_initializer(0., 0.02)
+    last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
+                                           strides=2,
+                                           padding='same',
+                                           kernel_initializer=initializer,
+                                           activation='tanh') 
 
-  initializer = tf.random_normal_initializer(0., 0.02)
-  last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
-                                         strides=2,
-                                         padding='same',
-                                         kernel_initializer=initializer,
-                                         activation='tanh')  
+    x = inputs
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
 
-  x = inputs
-  skips = []
-  for down in down_stack:
-    x = down(x)
-    skips.append(x)
+    skips = reversed(skips[:-1])
 
-  skips = reversed(skips[:-1])
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = tf.keras.layers.Concatenate()([x, skip])
 
-  for up, skip in zip(up_stack, skips):
-    x = up(x)
-    x = tf.keras.layers.Concatenate()([x, skip])
-
-  x = last(x)
-
-  return tf.keras.Model(inputs=inputs, outputs=x)
+    x = last(x)
+    return tf.keras.Model(inputs=inputs, outputs=x)
 
 generator = Generator()
 tf.keras.utils.plot_model(generator, show_shapes=True, dpi=64)
 
-def generator_loss(disc_generated_output, gen_output, target):
-  gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
-
-  l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
-
-  total_gen_loss = gan_loss + (LAMBDA * l1_loss)
-
-  return total_gen_loss, gan_loss, l1_loss
 
 loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
-LAMBDA = 100
+def generator_loss(disc_generated_output, gen_output, target):
+    gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+    l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+    total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+    return total_gen_loss, gan_loss, l1_loss
+
 
 def Discriminator():
-  initializer = tf.random_normal_initializer(0., 0.02)
-
-  inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
-  tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
-
-  x = tf.keras.layers.concatenate([inp, tar])  
-
-  down1 = downsample(64, 4, False)(x)  
-  down2 = downsample(128, 4)(down1)  
-  down3 = downsample(256, 4)(down2)  
-
-  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3) 
-  conv = tf.keras.layers.Conv2D(512, 4, strides=1,
-                                kernel_initializer=initializer,
-                                use_bias=False)(zero_pad1) 
-
-  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
-
-  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
-
-  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  
-
-  last = tf.keras.layers.Conv2D(1, 4, strides=1,
-                                kernel_initializer=initializer)(zero_pad2)  
-
-  return tf.keras.Model(inputs=[inp, tar], outputs=last)
+    initializer = tf.random_normal_initializer(0., 0.02)
+    inp = tf.keras.layers.Input(shape=[256, 256, 3], name='input_image')
+    tar = tf.keras.layers.Input(shape=[256, 256, 3], name='target_image')
+    x = tf.keras.layers.concatenate([inp, tar]) 
+    down1 = downsample(64, 4, False)(x)  
+    down2 = downsample(128, 4)(down1)  
+    down3 = downsample(256, 4)(down2)  
+    zero_pad1 = tf.keras.layers.ZeroPadding2D()(down3) 
+    conv = tf.keras.layers.Conv2D(512, 4, strides=1,
+                                  kernel_initializer=initializer,
+                                  use_bias=False)(zero_pad1) 
+    batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+    leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+    zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu) 
+    last = tf.keras.layers.Conv2D(1, 4, strides=1,
+                                  kernel_initializer=initializer)(zero_pad2) 
+    return tf.keras.Model(inputs=[inp, tar], outputs=last)
 
 discriminator = Discriminator()
 tf.keras.utils.plot_model(discriminator, show_shapes=True, dpi=64)
 
+
+def discriminator_loss(disc_real_output, disc_generated_output):
+    real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+    generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+    total_disc_loss = real_loss + generated_loss
+    return total_disc_loss
+
+
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+def train_step(input_image, target, epoch):
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        gen_output = generator(input_image, training=True)
+        disc_real_output = discriminator([input_image, target], training=True)
+        disc_generated_output = discriminator([input_image, gen_output], training=True)
+        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+        disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+    generator_gradients = gen_tape.gradient(gen_total_loss, generator.trainable_variables)
+    discriminator_gradients = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+    generator_optimizer.apply_gradients(zip(generator_gradients, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(discriminator_gradients, discriminator.trainable_variables))
+    return gen_total_loss, disc_loss
+
+def fit(train_ds, epochs):
+    for epoch in range(epochs):
+        start = time.time()
+        for example_input, example_target in train_ds.take(1):
+            generate_images(generator, example_input, example_target)
+        for input_image, target in train_ds:
+            gen_total_loss, disc_loss = train_step(input_image, target, epoch)
+        if (epoch + 1) % 20 == 0:
+            checkpoint.save(file_prefix=checkpoint_prefix)
+        print(f'Time taken for epoch {epoch + 1} is {time.time() - start} sec\n')
+
+def generate_images(model, test_input, tar):
+    prediction = model(test_input, training=True)
+    plt.figure(figsize=(15, 15))
+    display_list = [test_input[0], tar[0], prediction[0]]
+    title = ['Input Image', 'Ground Truth', 'Predicted Image']
+    for i in range(3):
+        plt.subplot(1, 3, i + 1)
+        plt.title(title[i])
+        plt.imshow(display_list[i] * 0.5 + 0.5)
+        plt.axis('off')
+    plt.show()
+
+train_dir = "path/to/train"
+image_pairs = [(os.path.join(train_dir, 'sar', name), os.path.join(train_dir, 'eo', name))
+               for name in os.listdir(os.path.join(train_dir, 'sar'))]
+train_dataset = create_dataset(image_pairs, is_training=True)
+fit(train_dataset, epochs=150)
