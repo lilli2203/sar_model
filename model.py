d8aff1b921daf4414e7cab69b7fe082d03a4e683
lilli2203 <mailyourlilli@gmail.com>
2024-08-04 00:43:17 +0530

    Define the generator with the downsampler and the upsampler:

--- model.py
+++ model.py
@@ -319,4 +319,69 @@ train_dataset = create_dataset(image_pairs, is_training=True)
 test_dataset = create_dataset(image_pairs, is_training=False)
 display_sample_images(train_dataset)
 
+ds = generate_train_dataset('/kaggle/input/ai-spacetech-hackathon/train')
 
+def separate_tensor(input):
+    real_image = tf.squeeze(input[0])
+    input_image = tf.squeeze(input[1])
+    return input_image, real_image
+
+train_samples = int(len(ds)*0.8)
+
+train_dataset = tf.data.Dataset.from_tensor_slices(ds[:train_samples])
+train_dataset = train_dataset.map(separate_tensor)
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.batch(BATCH_SIZE)
+
+test_dataset = tf.data.Dataset.from_tensor_slices(ds[train_samples:])
+test_dataset = test_dataset.map(separate_tensor)
+test_dataset = test_dataset.shuffle(BUFFER_SIZE)
+test_dataset = test_dataset.batch(BATCH_SIZE)
+
+#sample images
+inp = ds[25][1]
+re = ds[25][0]
+
+OUTPUT_CHANNELS = 3
+
+def downsample(filters, size, apply_batchnorm=True):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                             kernel_initializer=initializer, use_bias=False))
+
+  if apply_batchnorm:
+    result.add(tf.keras.layers.BatchNormalization())
+
+  result.add(tf.keras.layers.LeakyReLU())
+
+  return result
+
+down_model = downsample(3, 4)
+down_result = down_model(tf.expand_dims(inp, 0))
+print (down_result.shape)
+
+def upsample(filters, size, apply_dropout=False):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                    padding='same',
+                                    kernel_initializer=initializer,
+                                    use_bias=False))
+
+  result.add(tf.keras.layers.BatchNormalization())
+
+  if apply_dropout:
+      result.add(tf.keras.layers.Dropout(0.5))
+
+  result.add(tf.keras.layers.ReLU())
+
+  return result
+
+up_model = upsample(3, 4)
+up_result = up_model(down_result)
+print (up_result.shape)
