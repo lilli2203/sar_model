9d7fe40d6583cb2f5ec8405b19e5fc1f08f4478b
lilli2203 <mailyourlilli@gmail.com>
2024-08-04 00:41:37 +0530

    updated input pipeline

--- model.py
+++ model.py
@@ -214,28 +214,22 @@ BATCH_SIZE = 1
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 
-
-
 def resize(input_image, real_image, height, width):
     input_image = tf.image.resize(input_image, [height, width],
-                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                                  method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     real_image = tf.image.resize(real_image, [height, width],
-                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
+                                 method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     return input_image, real_image
 
 def random_crop(input_image, real_image):
     stacked_image = tf.stack([input_image, real_image], axis=0)
-    cropped_image = tf.image.random_crop(
-      stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
-
+    cropped_image = tf.image.random_crop(stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
     return cropped_image[0], cropped_image[1]
 
-# Normalizing the images to [-1, 1]
+
 def normalize(input_image, real_image):
     input_image = (input_image / 127.5) - 1
     real_image = (real_image / 127.5) - 1
-
     return input_image, real_image
 
 @tf.function()
@@ -264,5 +258,65 @@ def generate_train_dataset(train_dir):
         train_datapoint = [real_image, input_image]
         dataset.append(train_datapoint)
     return dataset
+
+def preprocess_image_train(image, label):
+    image, label = random_jitter(image, label)
+    image, label = normalize(image, label)
+    return image, label
+
+def preprocess_image_test(image, label):
+    image, label = resize(image, label, IMG_HEIGHT, IMG_WIDTH)
+    image, label = normalize(image, label)
+    return image, label
+
+def load_image_train(image_file):
+    input_image = tf.io.read_file(image_file[0])
+    input_image = tf.image.decode_jpeg(input_image)
+    real_image = tf.io.read_file(image_file[1])
+    real_image = tf.image.decode_jpeg(real_image)
+
+    input_image, real_image = preprocess_image_train(input_image, real_image)
+
+    return input_image, real_image
+
+def load_image_test(image_file):
+    input_image = tf.io.read_file(image_file[0])
+    input_image = tf.image.decode_jpeg(input_image)
+    real_image = tf.io.read_file(image_file[1])
+    real_image = tf.image.decode_jpeg(real_image)
+
+    input_image, real_image = preprocess_image_test(input_image, real_image)
+
+    return input_image, real_image
+
+def create_dataset(image_paths, is_training=True):
+    dataset = tf.data.Dataset.from_tensor_slices(image_paths)
+    if is_training:
+        dataset = dataset.map(load_image_train,
+                              num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    else:
+        dataset = dataset.map(load_image_test)
+
+    dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+    return dataset
+
+def display_sample_images(dataset):
+    for input_image, real_image in dataset.take(1):
+        plt.subplot(121)
+        plt.title('Input Image')
+        plt.imshow(input_image[0] * 0.5 + 0.5)
+
+        plt.subplot(122)
+        plt.title('Real Image')
+        plt.imshow(real_image[0] * 0.5 + 0.5)
+        plt.show()
+
+train_dir = ""
+image_pairs = [(os.path.join(train_dir, 'sar', name), os.path.join(train_dir, 'eo', name))
+               for name in os.listdir(os.path.join(train_dir, 'sar'))]
+
+train_dataset = create_dataset(image_pairs, is_training=True)
+test_dataset = create_dataset(image_pairs, is_training=False)
+display_sample_images(train_dataset)
 
 
