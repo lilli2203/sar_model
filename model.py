c4db7b5de6bc5d1a453c3d8ce8eed2d3233adc56
lilli2203 <mailyourlilli@gmail.com>
2024-08-04 00:34:48 +0530

    refined

--- model.py
+++ model.py
@@ -7,13 +7,18 @@ from urllib.error import HTTPError
 from zipfile import ZipFile
 import tarfile
 import shutil
+import logging
+import time
 
 CHUNK_SIZE = 40960
 DATA_SOURCE_MAPPING = 'ai-spacetech-hackathon:https%3A%2F%2Fstorage.googleapis.com%2Fkaggle-competitions-data%2Fkaggle-v2%2F72387%2F7958653%2Fbundle%2Farchive.zip%3FX-Goog-Algorithm%3DGOOG4-RSA-SHA256%26X-Goog-Credential%3Dgcp-kaggle-com%2540kaggle-161607.iam.gserviceaccount.com%252F20240316%252Fauto%252Fstorage%252Fgoog4_request%26X-Goog-Date%3D20240316T181238Z%26X-Goog-Expires%3D259200%26X-Goog-SignedHeaders%3Dhost%26X-Goog-Signature%3Da3817f5ea158a61dd6ef6afb073ea689108d63c5365e430796cab3d7f2e78c77cdbbd4421dc49383ade7efbda662260df14345f059ec3f31c30bd2f7b8f650410751c348e3c9fd99d6c84129eda111859963587d5387a7dd127e38367a6c3d13a83874df9feae02ea1ff5643649d2e822b57854f0574b1ea634d3d286a0e1ca98ed334dd7489fed4aa22b37e22d973dfb3d312b887ec135a8de907e22c5e38a0ff5f9624b216dfe22cb7ff4cf69f55d28312177a9e7c68094e693d5696b4daf9c15e6c1fac3d15937bc86c965f4c944e392b8e2182ae3118fbd39efcb4aab09f52f383aa11633b05fff13f7df92d4f16c50294878a0957cad170ca51d20c874e'
 
-KAGGLE_INPUT_PATH='/kaggle/input'
-KAGGLE_WORKING_PATH='/kaggle/working'
-KAGGLE_SYMLINK='kaggle'
+KAGGLE_INPUT_PATH = '/kaggle/input'
+KAGGLE_WORKING_PATH = '/kaggle/working'
+KAGGLE_SYMLINK = 'kaggle'
+
+# Set up logging
+logging.basicConfig(filename='download.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 !umount /kaggle/input/ 2> /dev/null
 shutil.rmtree('/kaggle/input', ignore_errors=True)
@@ -21,44 +26,54 @@ os.makedirs(KAGGLE_INPUT_PATH, 0o777, exist_ok=True)
 os.makedirs(KAGGLE_WORKING_PATH, 0o777, exist_ok=True)
 
 try:
-  os.symlink(KAGGLE_INPUT_PATH, os.path.join("..", 'input'), target_is_directory=True)
+    os.symlink(KAGGLE_INPUT_PATH, os.path.join("..", 'input'), target_is_directory=True)
 except FileExistsError:
-  pass
+    pass
 try:
-  os.symlink(KAGGLE_WORKING_PATH, os.path.join("..", 'working'), target_is_directory=True)
+    os.symlink(KAGGLE_WORKING_PATH, os.path.join("..", 'working'), target_is_directory=True)
 except FileExistsError:
-  pass
+    pass
 
 for data_source_mapping in DATA_SOURCE_MAPPING.split(','):
     directory, download_url_encoded = data_source_mapping.split(':')
     download_url = unquote(download_url_encoded)
-    filename = urlparse(download_url).path
+    filename = os.path.basename(urlparse(download_url).path)
     destination_path = os.path.join(KAGGLE_INPUT_PATH, directory)
+
     try:
         with urlopen(download_url) as fileres, NamedTemporaryFile() as tfile:
-            total_length = fileres.headers['content-length']
-            print(f'Downloading {directory}, {total_length} bytes compressed')
+            total_length = int(fileres.headers.get('content-length', 0))
+            logging.info(f'Downloading {directory}, {total_length} bytes compressed')
             dl = 0
-            data = fileres.read(CHUNK_SIZE)
-            while len(data) > 0:
+            start_time = time.time()
+            while True:
+                data = fileres.read(CHUNK_SIZE)
+                if not data:
+                    break
                 dl += len(data)
                 tfile.write(data)
-                done = int(50 * dl / int(total_length))
-                sys.stdout.write(f"\r[{'=' * done}{' ' * (50-done)}] {dl} bytes downloaded")
+                elapsed_time = time.time() - start_time
+                speed = dl / elapsed_time if elapsed_time > 0 else 0
+                done = int(50 * dl / total_length)
+                sys.stdout.write(f"\r[{'=' * done}{' ' * (50 - done)}] {dl} bytes downloaded, {speed:.2f} bytes/sec")
                 sys.stdout.flush()
-                data = fileres.read(CHUNK_SIZE)
+            tfile.flush()
+
             if filename.endswith('.zip'):
-              with ZipFile(tfile) as zfile:
-                zfile.extractall(destination_path)
+                with ZipFile(tfile.name) as zfile:
+                    zfile.extractall(destination_path)
             else:
-              with tarfile.open(tfile.name) as tarfile:
-                tarfile.extractall(destination_path)
+                with tarfile.open(tfile.name) as tarfile_obj:
+                    tarfile_obj.extractall(destination_path)
             print(f'\nDownloaded and uncompressed: {directory}')
+            logging.info(f'Downloaded and uncompressed: {directory}')
+
     except HTTPError as e:
+        logging.error(f'Failed to load (likely expired) {download_url} to path {destination_path}, HTTPError: {e}')
         print(f'Failed to load (likely expired) {download_url} to path {destination_path}')
-        continue
     except OSError as e:
+        logging.error(f'Failed to load {download_url} to path {destination_path}, OSError: {e}')
         print(f'Failed to load {download_url} to path {destination_path}')
-        continue
 
 print('Data source import complete.')
+logging.info('Data source import complete.')
